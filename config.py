33e29899b541e91af0a1a9278af046bdcdb5d14e
suspiciousboy-bots <suspiciousboy1412@gmail.com>
2026-06-30 21:32:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
-# You can either use this or environment variables
-BOT_TOKEN = ""  # Will be set via environment
-API_ID = ""
-API_HASH = ""
+import os
+
+class Config:
+    # Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", "")
+    OWNER_NAME = os.environ.get("OWNER_NAME", "⏤͟͞ 𝐂𝐑𝐀𝐙𝐘 𝐁𝐎𝐘 ᭄࿐")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 7790607144))
+    
+    # Optional
+    BOT_NAME = os.environ.get("BOT_NAME", "「Pɪᴋᴀᴄʜᴜ」")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Pikachuu_Robot")
+    SESSION_DIR = os.environ.get("SESSION_DIR", "/tmp/userbot_sessions")
+
+config = Config()
